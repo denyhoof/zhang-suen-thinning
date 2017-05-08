ed795613df5975b2006475739145af9212caa245
20d_kon <denis.kondranin@graphics.cs.msu.ru>
2017-05-09 00:57:48 +0300

    Speed optimization

--- thinning.py
+++ thinning.py
@@ -3,59 +3,66 @@ import cv2
 import sys
 
 
+def _g(X, Y, Z):
+    return np.logical_and(np.logical_not(X), np.logical_or(Y, Z))
+
+
+def _f(A, B, C, D):
+    return np.logical_and(np.logical_or(A, np.logical_or(B, np.logical_not(C))), D)
+
+
 def _thinningIteration(image, iter):
-    marker = np.zeros(image.shape, dtype=np.uint8)
-    for i in range(1, image.shape[0] - 1):
-        for j in range(1, image.shape[1] - 1):
-            p2 = image[i-1, j]
-            p3 = image[i-1, j+1]
-            p4 = image[i, j+1]
-            p5 = image[i+1, j+1]
-            p6 = image[i+1, j]
-            p7 = image[i+1, j-1]
-            p8 = image[i, j-1] 
-            p9 = image[i-1, j-1]  
-
-            C  = (not p2 and (p3 or p4)) + (not p4 and (p5 or p6)) + \
-                     (not p6 and (p7 or p8)) + (not p8 and (p9 or p2))
-            N1 = (p9 or p2) + (p3 or p4) + (p5 or p6) + (p7 or p8)
-            N2 = (p2 or p3) + (p4 or p5) + (p6 or p7) + (p8 or p9)
-            N  = min(N1, N2)
-            m = 0
-            if iter == 0:
-                m = ((p6 or p7 or not p9) and p8)
-            else:
-                m = ((p2 or p3 or not p5) and p4)
-
-            if C == 1 and (N >= 2 and N <= 3) and m == 0:
-                marker[i,j] = 1;
-
-    return image * (1 - marker)
+    h, w = image.shape
+    P2 = image[0:h-2, 1:w-1]
+    P3 = image[0:h-2, 2:w]
+    P4 = image[1:h-1, 2:w]
+    P5 = image[2:h, 2:w]
+    P6 = image[2:h, 1:w-1]
+    P7 = image[2:h, 0:w-2]
+    P8 = image[1:h-1, 0:w-2]
+    P9 = image[0:h-2, 0:w-2]
+    C = _g(P2, P3, P4).astype(np.uint8) + _g(P4, P5, P6).astype(np.uint8) + \
+        _g(P6, P7, P8).astype(np.uint8) + _g(P8, P9, P2).astype(np.uint8)
+    N1 = np.logical_or(P9, P2).astype(np.uint8) + np.logical_or(P3, P4).astype(np.uint8) + \
+         np.logical_or(P5, P6).astype(np.uint8) + np.logical_or(P7, P8).astype(np.uint8)
+    N2 = np.logical_or(P2, P3).astype(np.uint8) + np.logical_or(P4, P5).astype(np.uint8) + \
+         np.logical_or(P6, P7).astype(np.uint8) + np.logical_or(P8, P9).astype(np.uint8)
+    N = np.minimum(N1, N2)
+    M = _f(P2, P3, P5, P4) if iter else _f(P6, P7, P9, P8)
+    MR = np.zeros((h, w))
+    MR[1:h-1, 1:w-1] = np.logical_and(
+                           np.logical_and(
+                               np.logical_and(
+                                   np.where(C == 1, 1, 0), 
+                                   np.where(N >= 2, 1, 0)), 
+                               np.where(N <= 3, 1, 0)), 
+                           np.where(M == 0, 1, 0))
+    return image * (1 - MR)
 
 
 def thinning(src):
-	dst = src.copy() / 255
-	prev = np.zeros(src.shape[:2], np.uint8)
-	diff = None
+    dst = src.copy() / 255
+    prev = np.zeros(src.shape[:2], np.uint8)
+    diff = None
 
-	while True:
-		dst = _thinningIteration(dst, 0)
-		dst = _thinningIteration(dst, 1)
-		diff = np.absolute(dst - prev)
-		prev = dst.copy()
-		if np.sum(diff) == 0:
-			break
-	return dst * 255
+    while True:
+        dst = _thinningIteration(dst, 0)
+        dst = _thinningIteration(dst, 1)
+        diff = np.absolute(dst - prev)
+        prev = dst.copy()
+        if np.sum(diff) == 0:
+            break
+    return dst * 255
 
 
 if __name__ == "__main__":
-	src = cv2.imread("kanji.bmp")
-	if src is None:
-		sys.exit()
-	bw = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-	_, bw2 = cv2.threshold(bw, 10, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-	bw2 = thinning(255 - bw2)
-	cv2.imshow("src", bw)
-	cv2.imshow("thinning", bw2)
-	cv2.waitKey()
+    src = cv2.imread("kanji.bmp")
+    if src is None:
+        sys.exit()
+    bw = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    _, bw2 = cv2.threshold(bw, 10, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    bw2 = thinning(255 - bw2)
+    cv2.imshow("src", bw)
+    cv2.imshow("thinning", bw2)
+    cv2.waitKey()
 
